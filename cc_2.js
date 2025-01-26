db9622537a635121625fce00b30671cfddc2bed2
ashlym1 <ashlym3@usf.edu>
2025-01-26 14:30:16 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,9 @@
+// Task 1: Working with Arrays - Store Inventory
+
+let products = ["Ceremonial Grade Matcha Powder", "Green Tea Wafer Cookies", "Green Tea Milk Spread", "Hibiscus Tea Bags", "Strawberry Wafer Cookies"];  // Array of Products
+
+products.push("Strawberry Matcha Powder");  // Add a new product to the end of the array
+products.pop();  // Remove the last product from the array
+
+console.log(products);  // Log the updated product list to the console
+
